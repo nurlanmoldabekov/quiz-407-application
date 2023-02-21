031550d662b9a9e3089ddd06ee364f72f89659bf
nurlan <nurlanmol@nitka.com>
2023-02-21 14:32:17 -0500

    adding withoutSpringBoot functionality

--- README.md
+++ README.md
@@ -2,5 +2,14 @@
 
 #### How to start the application
 
-From command-line, from the root directory, run `mvn clean compile package`
-Then `java `
+From command-line, from the root directory, run `mvn clean compile package`.
+
+Then `java -jar ./targer/router-application`.
+
+Swagger URL: http://localhost:8080/swagger-ui/index.html
+
+Alternatively, you can execute it without Spring Boot:
+
+`java -jar ./target/route-application.jar withoutSpringBoot "QEW" "Highway 403"`.
+
+Passing `withoutSpringBoot` as 1st argument, `from` and `to` as 2nd and 3rd. 
--- src/main/java/com/example/quiznurlanmoldabekov/Application.java
+++ src/main/java/com/example/quiznurlanmoldabekov/Application.java
@@ -1,13 +1,40 @@
 package com.example.quiznurlanmoldabekov;
 
+import com.example.quiznurlanmoldabekov.config.RouteConfig;
+import com.example.quiznurlanmoldabekov.router.RouterService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigDecimal;
+import java.util.Properties;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        if (args != null && args.length > 0 && args[0].equals("withoutSpringBoot")) {
+            var mapper = new ObjectMapper();
+            var config = new RouteConfig();
+            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            try (InputStream inputStream = new FileInputStream("src/main/resources/application.properties")) {
+                Properties prop = new Properties();
+                prop.load(inputStream);
+                config.setCostPerKm(BigDecimal.valueOf(Double.parseDouble(prop.getProperty("route.cost-per-km"))));
+                var service = new RouterService(mapper, config);
+                var res = service.calculate(args[1], args[2]);
+                System.out.println("distance: " + res.getDistance());
+                System.out.println("cost: " + res.getCost());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            SpringApplication.run(Application.class, args);
+        }
     }
 
 }
